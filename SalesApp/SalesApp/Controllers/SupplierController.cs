6edf12ba9f56235be2c8afd5515466f1ce519a4e
bandilemashile <35614625+bandilemashile@users.noreply.github.com>
2020-11-02 21:35:17 +0200

    Exercise-3-Upload-1

---
+++ SalesApp/SalesApp/Controllers/SupplierController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Data;
+using System.Web.Http;
+using SupplierData;
+using System.Data.Entity;
+
+
+
+
+namespace SalesApp.Controllers
+{
+    public class SupplierController : ApiController
+    {
+
+        public IEnumerable<tblSupplier> Get()
+        {
+            using (SupplierEntities1 entities1 = new SupplierEntities1())
+            {
+               return entities1.tblSuppliers.ToList();
+            }
+        }
+
+       public tblSupplier Get(int id)
+        {
+            using (SupplierEntities1 entities1 = new SupplierEntities1())
+            {
+                return entities1.tblSuppliers.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
